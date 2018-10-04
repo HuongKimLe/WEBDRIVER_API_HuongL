806eea378b5c96783151566cb1efd1e3bdc04a6f
huong.le <huong.le@HUONGLHCM.mshome.net>
2018-10-04 15:09:24 +0700

    finish ex03

--- src/selenium_api/exercise03.java
+++ src/selenium_api/exercise03.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 
 public class exercise03 {
@@ -17,10 +18,10 @@ public class exercise03 {
 	
   @BeforeClass
   public void beforeClass() {
-	
+	  driver= new FirefoxDriver();	
 	 
 	  }
-  @Test
+  @Test(enabled = false)
   public void CheckElementDisplay() {
 	  
 	  
@@ -42,19 +43,97 @@ public class exercise03 {
 	 
 	WebElement ageRadioButton = driver.findElement(By.xpath("//input[@id='under_18']"));
 		if(ageRadioButton.isDisplayed()) {
-			ageRadioButton.clear();
+			ageRadioButton.click();
 		}
   }
 
-  @Test
-  public void CheckElementenableorDisable() {
+  @Test(enabled = false)
+  public void CheckElementenableorEnable() {
+	  
+	  //Step 1: truy cap den trang http://daominhdam.890m.com/
+	  
+	  driver.get("http://daominhdam.890m.com/");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.manage().window().maximize();
+	  
+	  // Step 2: kiem tra cac phan tu enable tren trang: Email/ Age (Under 18)/ Education/ Job Role 01/ Interests (Development)/ Slider 01/ Button is enabled
+	  // Email
+	  WebElement emalTextBox2 = driver.findElement(By.xpath("//input[@id='mail']"));
+	  isElementEnable(emalTextBox2);
+	  //Age (Under 18)
+	  WebElement ageRadioButton2 = driver.findElement(By.xpath("//input[@id='under_18']"));
+	  isElementEnable(ageRadioButton2);
+				
+		// Education
+		WebElement educationTextAre2 = driver.findElement(By.xpath("//textarea[@id='edu']"));
+		isElementEnable(educationTextAre2);
+		//Job Role 01
+		
+		WebElement Job1Select = driver.findElement(By.xpath("//select[@id='job1']"));
+		isElementEnable(Job1Select);
+	 
+		//Interests (Development)
+		WebElement Interestcheckbox = driver.findElement(By.xpath("//label[text()='Development' and @class ='light']"));
+		isElementEnable(Interestcheckbox);
+	  
+		//
+		WebElement Slide01Highlight = driver.findElement(By.xpath("//input[@id='slider-1']"));
+		isElementEnable(Slide01Highlight);
+		//
+		WebElement ButtonisEnable = driver.findElement(By.xpath("//button[@id='button-enabled']"));
+		isElementEnable(ButtonisEnable);
   }
+	
+  @Test
+public void CheckElementenableorDisable() {
+	  
+	  driver.get("http://daominhdam.890m.com/");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.manage().window().maximize();
+		
+		// Kiểm tra các phần tử sau disable trên trang: Password / Age (Radiobutton is disabled)/ Biography/ 
+		// Job Role 02/ Interests (Checkbox is disabled)/ Slider 02/ Button is disabled
+		
+		
+		WebElement PassTextBox = driver.findElement(By.xpath("//input[@id='password']"));
+		
+		WebElement AgeRadioButtonDisable = driver.findElement(By.xpath("//input[@id='radio-disabled']"));
+		
+		WebElement BiographyTextAre = driver.findElement(By.xpath("//textarea[@id=\"bio\"]"));
+		
+		WebElement Job2Select = driver.findElement(By.xpath("//select[@id='job2']"));
+		
+		WebElement InterestcheckboxDisable = driver.findElement(By.xpath("//label[text()='Checkbox is disabled']"));
+		
+		WebElement Slide02Highlight = driver.findElement(By.xpath("//input[@id='slider-2']"));
+		
+		WebElement ButtonisDisable = driver.findElement(By.xpath("//button[@id='button-disabled']"));
+		
+		isElementEnable(PassTextBox);
+		isElementEnable(AgeRadioButtonDisable);
+		isElementEnable(BiographyTextAre);
+		isElementEnable(Job2Select);
+		isElementEnable(InterestcheckboxDisable);
+		isElementEnable(Slide02Highlight);
+		isElementEnable(ButtonisDisable);
+}
+		
+	  
   @Test
   public void CheckElementSelected() {
   }
 
   @AfterClass
   public void afterClass() {
+	//  driver.quit();
   }
-
-}
+  
+  public void isElementEnable(WebElement element) {
+	  if (element.isEnabled()) {
+		  System.out.println("Element is enabled");
+	  } else {
+		  System.out.println("Element is disabled");
+	  }
+  }
+  
+ }
