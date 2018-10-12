1a5aa7d139ab7d4541ff9815e3c6cd960497b0af
huong.le <huong.le@HUONGLHCM.mshome.net>
2018-10-12 14:26:52 +0700

    finish handle dropdown

--- src/selenium_api/Topic05_HandleDropdown.java
+++ src/selenium_api/Topic05_HandleDropdown.java
@@ -1,12 +1,10 @@
 package selenium_api;
 
+import org.testng.annotations.Test;
+
+
 import org.testng.annotations.BeforeClass;
 
-
-
-
-
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -31,11 +29,8 @@ public class Topic05_HandleDropdown {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
-		
-		
-	
 	}
-
+@Test
 	public void TS01DropdownList() {
 		
 		//Step 01
@@ -48,26 +43,30 @@ public class Topic05_HandleDropdown {
 		select.selectByVisibleText("Automation Tester");
 		
 		//Step 04 - Kiểm tra giá trị đã được chọn thành công
+		System.out.println(select.getFirstSelectedOption().getText());
 		Assert.assertEquals(select.getFirstSelectedOption().getText(), "Automation Tester");
 		
 		//Step 05 - Chọn giá trị Manual Tester trong dropdown bằng phương thức selectValue
 		select.selectByValue("manual");
 		
 		//Step 06 - Kiểm tra giá trị đã được chọn thành công
+		System.out.println(select.getFirstSelectedOption().getText());
 		Assert.assertEquals(select.getFirstSelectedOption().getText(), "Manual Tester");
 		
 		//Step 07 - Chọn giá trị Mobile Tester trong dropdown bằng phương thức selectIndex		
 		select.selectByIndex(3);
 		
 		//Step 08 - Kiểm tra giá trị đã được chọn thành công
-		Assert.assertEquals(select.getFirstSelectedOption().getText(), "Mobile Teste");
+		System.out.println(select.getFirstSelectedOption().getText());
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), "Mobile Tester");
 		
 		//Step 09 - Kiểm tra dropdown có đủ 5 giá trị
-		select.getAllSelectedOptions();
-		Assert.assertEquals(select.getAllSelectedOptions(), 5);
+		System.out.println(select.getOptions().size());		
+		Assert.assertEquals(select.getOptions().size(), 5);
 		
 		
 	}
+@Test
     public void TS02CustomerDropdown() {
 		
 	}
@@ -76,11 +75,5 @@ public class Topic05_HandleDropdown {
 	public void afterClass() {
 	}
 
-	public int randomEmail() {
-		Random rand = new Random();
-		int number = rand.nextInt(10000);
-		return number;
-
-	}
 
 }
